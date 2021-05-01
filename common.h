18622deeae1c88f24d08bc79a1416525a769443a
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-02 00:03:01 +0300

    Create common.h

---
+++ common.h
@@ -0,0 +1,12 @@
+#ifndef common_h
+#define common_h
+#include<iostream>
+using namespace std;
+#include "iTax.h"
+class common :public iTax
+{
+public:
+	void operator*();
+		void showc();
+}
+#endif
