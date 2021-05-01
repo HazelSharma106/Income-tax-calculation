2db3435a145aec6fc16c716da3640dbcc929e308
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-02 00:04:45 +0300

    Create p1.h

---
+++ p1.h
@@ -0,0 +1,15 @@
+#ifndef p1_h
+#define p1_h
+#include<iostream>
+using namespace std;
+#include "iTax.h"
+class p1 :public iTax
+{
+public:
+	void getz();
+	void print1();
+	void showz();
+   
+  
+};
+#endif
