c878c6a02c624a54d9f0d765d880fe48a7163243
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-02 13:21:02 +0300

    Update iTax.cpp

--- iTax.cpp
+++ iTax.cpp
@@ -4,22 +4,22 @@ using namespace std;
 class iTax
 {
 public:
-    char* name = new char[90];
+    char* name = new char[90]; //memory allocation 
 
-    char* occ = new char[90];
+    char* occ = new char[90]; //memory allocation
 
     int ID;
 
-    virtual void getit(const char* a)
+    virtual void getit(const char* a) // passing a pointer
     {
-        strcpy(occ, a);
+        strcpy(occ, a); //  Strcpy -It is used to copy one string to another. In C language
         cout << "Enter your name: ";
-        gets_s(name);
+        gets_s(name); // gets - The gets() function reads characters from stdin and stores them in str until a newline character or end of file is found
         cout << "Enter your ID: ";
         cin >> ID;
     }
 
-    virtual void showit()
+    virtual void showit() // showing of the data
     {
 
         cout << "\nName    : " << name;
