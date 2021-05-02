933eb13120c21d24c03bf9b350b2fedc1c14f4b4
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-02 13:32:44 +0300

    Update p20.cpp

--- p20.cpp
+++ p20.cpp
@@ -5,21 +5,21 @@ class p20 :public iTax
 {
 public:
     double sal, tax;
-    p20()
+    p20() // default constructor
     {
     }
-    void getty()
+    void getty() // entering of data
     {
         cout << "Enter salary in a year: ";
         cin >> sal;
         tax = sal * (.20);
     }
-    void print3()
+    void print3() // printing of data
     {
 
         cout << "The income tax is: " << tax << "tk." << endl;
     }
-    void showty()
+    void showty() // showing of data
     {
         cout << "\nSalary  : " << " tk. in a year" << sal;
         cout << "\nTax     : " << tax << " tk." << "\n\n";
