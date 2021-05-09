7ef92e0f7daa23198dcf41ad9c992adf6a9c5747
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-09 18:32:21 +0300

    Update p25.h

--- p25.h
+++ p25.h
@@ -1,31 +1,16 @@
-#include"p25.h"
+#ifndef p25_h
+#define p25_h
 #include<iostream>
 using namespace std;
-class p25 :public iTax
+#include"getshow.h"
+class p25 :public getshow //abstraction
 {
-private:
-    double s, t;
-
 public:
     double sal, tax;
 
-    void gettf()
-    {
-        cout << "Enter salary in a year: ";
-        cin >> sal;
-        tax = sal * (.25);
-        s = sal;
-        t = tax;
 
-    }
+    void gettf();
+    void showtf();
 
-    void print4()
-    {
-        cout << "The income tax is: " << tax << "tk." << endl;
-    }
-    void showtf()
-    {
-        cout << "\nSalary      : " << " tk. in a year" << sal;
-        cout << "\nTax         : " << tax << " tk." << "\n\n";
-    }
 };
+#endif
