e57ea9fb12942d65ea066a17c4722dc197f21d59
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-09 18:27:35 +0300

    Update p15.cpp

--- p15.cpp
+++ p15.cpp
@@ -1,30 +1,25 @@
 #include"p15.h"
 #include<iostream>
 using namespace std;
-class p15 :public iTax
-{
-public:
-    double sal, tax;
-    p15() // default constructor
-    {
 
-    }
-    double getfn()
+
+    void p15:: getfn()
     {
 
         cout << "Enter salary in a year: ";
         cin >> sal;
-        tax = sal * (.15); // entering of data
-
+        tax = sal + (150000);
     }
-    void print2()
+    p15 p15::operator+(const p15& p) // operator overloading
     {
-        cout << "The income tax is: " << tax << "tk." << endl; // printing of data
+        
+        p15 P;
+        P.tax = this->tax + p.tax;
+        cout << "The income tax is: " << tax << "tk." << endl; \
+        return P;
     }
-
-    void showfn()
+    void p15:: showfn()
     {
-        cout << "\nSalary  : " << " tk. in a year" << sal; // displaying the data
+        cout << "\nSalary  : " << " tk. in a year" << sal;
         cout << "\nTax     : " << tax << " tk." << "\n\n";
     }
-};
