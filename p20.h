c401e83149365c1d89a82af577a3cafca729d0a2
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-09 18:29:21 +0300

    Update p20.h

--- p20.h
+++ p20.h
@@ -1,27 +1,19 @@
-#include"p20.h"
+#ifndef p20_h
+#define p20_h
 #include<iostream>
 using namespace std;
-class p20 :public iTax
+#include"getshow.h"
+class p20 :public getshow // abstraction
 {
+
 public:
     double sal, tax;
-    p20()
-    {
-    }
-    void getty()
-    {
-        cout << "Enter salary in a year: ";
-        cin >> sal;
-        tax = sal * (.20);
-    }
-    void print3()
-    {
 
-        cout << "The income tax is: " << tax << "tk." << endl;
-    }
-    void showty()
-    {
-        cout << "\nSalary  : " << " tk. in a year" << sal;
-        cout << "\nTax     : " << tax << " tk." << "\n\n";
-    }
+
+    void getty();
+    p20 operator+(const p20& q); // operator overloading
+   
+    void showty();
 };
+#endif
+
