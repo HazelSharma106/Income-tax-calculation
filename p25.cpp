6878919fac9de44275b02181373ed2f96a3c50ca
HazelSharma106 <78410708+HazelSharma106@users.noreply.github.com>
2021-05-09 18:31:55 +0300

    Update p25.cpp

--- p25.cpp
+++ p25.cpp
@@ -1,34 +1,29 @@
 #include"p25.h"
 #include<iostream>
 using namespace std;
-class p25 :public iTax
-{
-private:
-    double s, t;
 
-public:
-    double sal, tax;
-    p25() // default constructor
-    {
 
-    }
-    void gettf()
+
+    void p25::gettf()
     {
-        cout << "Enter salary in a year: "; // enetering of data
+        cout << "Enter salary in a year: ";
         cin >> sal;
-        tax = sal * (.25);
-        s = sal;
-        t = tax;
-
-    }
-
-    void print4() // printing of data
-    {
+        tax = sal + (25000000);
         cout << "The income tax is: " << tax << "tk." << endl;
     }
-    void showtf() // showing of data
+    void p25::showtf()
     {
         cout << "\nSalary      : " << " tk. in a year" << sal;
         cout << "\nTax         : " << tax << " tk." << "\n\n";
+    }
+class greeting //polymorphism
+{
+    void show()
+    {
+        cout << "welcome" << endl; // show function is overloaded
+    }
+    void show(int a)
+    {
+        cout << "thank you" << endl;
     }
 };
